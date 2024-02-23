25b57908dadce6b2ab401890e645d5a0f95329ff
Your Parsa <parypary82@gmail.com>
2024-02-23 14:43:05 +0330

    fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="relative h-full w-full bg-zinc-950 flex flex-col justify-start items-center p-4 text-slate-100 space-y-12">
       <h1 className="sm:text-8xl text-5xl mt-10 animate-colorize">Accessory</h1>
-      <span className="text-5xl">
+      <span className="text-4xl">
         Location: <span className="text-red-500">Tabriz</span>
       </span>
       <p className="animate-pulse text-4xl text-blue-300">Coming soon...</p>
