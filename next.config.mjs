c932f07b4d2ce1fcb8e4ee3f1d1f18a134a04bb6
Your Parsa <parypary82@gmail.com>
2024-02-23 14:35:22 +0330

    my first pwa app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  // disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/",
+  // sw: "service-worker.js",
+  //...
+  workboxOptions: {
+    // Workbox options go here...
+  },
+  // customWorkerSrc: "service-worker",
+  // customWorkerDest: "somewhere-else", // defaults to `dest`
+  // customWorkerPrefix: "not/a-worker",
+  // ...
+  fallbacks: {
+    // Failed page requests fallback to this.
+    document: "/~offline",
+    // This is for /_next/.../.json files.
+    // data: "/fallback.json",
+    // This is for images.
+    // image: "/fallback.webp",
+    // This is for audio files.
+    // audio: "/fallback.mp3",
+    // This is for video files.
+    // video: "/fallback.mp4",
+    // This is for fonts.
+    // font: "/fallback-font.woff2",
+  },
+});
+
+export default withPWA({
+  // Your Next.js config
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,27 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
+      animation: {
+        colorize: "changeColor 15s linear infinite",
+      },
+      keyframes: {
+        changeColor: {
+          "0%": { color: "orange" },
+          "10%": { color: "red" },
+          "20%": { color: "yellow" },
+          "30%": { color: "purple" },
+          "40%": { color: "green" },
+          "50%": { color: "lightblue" },
+          "60%": { color: "blue" },
+          "70%": { color: "darkblue" },
+          "80%": { color: "yellow" },
+          "90%": { color: "red" },
+          "100%": { color: "orange" },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
